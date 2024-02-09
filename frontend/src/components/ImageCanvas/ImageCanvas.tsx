4bfbdf75910df55fc13523e600ae0b1ba51bfc1e
Revchenko Dmitry <reuchankadzmitry@gmail.com>
2024-02-09 18:29:58 +0300

    fix canvas

--- frontend/src/components/ImageCanvas/ImageCanvas.tsx
+++ frontend/src/components/ImageCanvas/ImageCanvas.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, MutableRefObject } from "react";
+import { useRef, MutableRefObject } from "react";
 
 type ImageCanvasProps = {
     imageUrl: string;
@@ -10,15 +10,23 @@ export function ImageCanvas({ imageUrl }: ImageCanvasProps) {
     // let img: HTMLImageElement | null = null;
     const canvasRef = useRef<HTMLCanvasElement>() as MutableRefObject<HTMLCanvasElement>;
 
-    useEffect(() => {
-        const img = new Image();
-        img.src = imageUrl;
-        const canvas = canvasRef.current as unknown;
-        if (canvas instanceof HTMLCanvasElement) {
-            const ctx = canvas.getContext("2d");
-            ctx?.drawImage(img, 0, 0, img.width, img.height);
-        }
-    }, [imageUrl]);
+    const img = new Image();
+    img.src = imageUrl;
+    const canvas = canvasRef.current as unknown;
+    if (canvas instanceof HTMLCanvasElement) {
+        const ctx = canvas.getContext("2d");
+        ctx?.drawImage(img, 0, 0, img.width, img.height);
+    }
+
+    // useEffect(() => {
+    //     const img = new Image();
+    //     img.src = imageUrl;
+    //     const canvas = canvasRef.current as unknown;
+    //     if (canvas instanceof HTMLCanvasElement) {
+    //         const ctx = canvas.getContext("2d");
+    //         ctx?.drawImage(img, 0, 0, img.width, img.height);
+    //     }
+    // }, [imageUrl]);
 
     return <canvas ref={canvasRef} className="product__image" width={500} height={500}></canvas>;
 }
